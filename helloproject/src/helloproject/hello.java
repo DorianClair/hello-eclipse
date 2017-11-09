9cfebf58451064cb9723e4f665b66463de5d8f1b
DorianClair <Dorianclair67@gmail.com>
2017-11-09 15:41:50 -0400

    2nd try

---
+++ helloproject/src/helloproject/hello.java
@@ -0,0 +1,10 @@
+package helloproject;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my nickname is dorian");
+	}
+
+}
